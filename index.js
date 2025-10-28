0ecae30043050e1a4b5fe09fdcc321866fa01901
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-28 10:59:56 +0700

    mendambah get, post, put, dan delete kedalam index.js

--- index.js
+++ index.js
@@ -31,4 +31,49 @@ app.get('/komik', async (req, res) => {
     }
 });
 
-app.post
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komiks);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+        await komik.update(data);
+        res.send({ message: 'Komik updated successfully', komik });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+        await komik.destroy();
+        res.send({ message: 'Komik deleted successfully' });
+    } catch (err) {
+        res.status(500).send(err);
+         }
+});
+        
+
+
+
+    
