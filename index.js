d50ceb13d138121e859940389452b9e076f80750
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-10-28 09:57:51 +0700

    konfigurasi port ke db

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: true
+ }));
+
+ app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
